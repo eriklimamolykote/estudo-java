9c5ea3408bea807a7be30e3d64f0d24148d6f42a
eriklimamolykote <eriklim@hotmail.com>
2022-11-14 10:44:28 -0300

    commit-estudo-java-14-11-2022

--- polymorphism/Payable.java
+++
@@ -1,9 +0,0 @@
-package polymorphism;
-// Fig. 10.11: Payable.java
-// Payable interface declaration.
-
-public interface Payable {
-
-    double getPaymentAmount(); // calculate payment; no implementation
-    
-}
